c45dfe7ff573bf8e8956e914d79328a54274759d
wendymunyasi <wendymunyasi@gmail.com>
2020-10-15 20:50:27 +0300

    display list of products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,8 @@
+import { Product } from './product';
 import { Album } from './album';
-// import { Http, Response } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { Http } from '@angular/common/http';
 import 'rxjs/add/operator/map';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 
@@ -12,6 +11,7 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(
     private _http: Http
@@ -21,8 +21,8 @@ export class ProductService {
     return this._http.get(this._albumUrl).map(response => <Album>response.json() as Album)
   }
 
-  // getAlbum(id: number){
-  //   return this._http.get(this._albumUrl).map(respone => response.json());
-  // }
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json())
+  }
 
 }
